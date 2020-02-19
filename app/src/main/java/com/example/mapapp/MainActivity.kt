ed8408a1eef05aaeda4728fd39ae89205a20796a
jan <fortnitetest@mail.de>
2020-02-19 18:46:08 +0100

    changed ListView items to json entries (no declared array anymore)

--- app/src/main/java/com/example/mapapp/MainActivity.kt
+++ app/src/main/java/com/example/mapapp/MainActivity.kt
@@ -5,8 +5,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.GridView
 import android.widget.ListView
 import android.widget.Toast
+import org.json.JSONObject
+import java.io.InputStream
 
 
 const val  CATEGORY_NAME = "com.example.mappapp.CATEGORY_NAME"
@@ -17,13 +20,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val json = parseJSON("data.json")
+        val rawJSOn = json.getJSONObject("data")
+        val categories = rawJSOn.keys()
+        var categoriesArray: ArrayList<String> = ArrayList()
+        categories.forEach { categoriesArray.add(it) }
+
         val listView: ListView = findViewById(R.id.main_listview)
-        val subjects = arrayOf("biology", "math", "physics")
-        listView.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, subjects)
+        listView.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, categoriesArray)
 
         listView.setOnItemClickListener { adapterview, view, i, l ->
-            Toast.makeText(applicationContext, subjects[i], Toast.LENGTH_SHORT).show()
-            showSubCategories(subjects[i])
+            Toast.makeText(applicationContext, categoriesArray[i], Toast.LENGTH_SHORT).show()
+            showSubCategories(categoriesArray[i])
         }
     }
 
@@ -32,5 +40,12 @@ class MainActivity : AppCompatActivity() {
             putExtra(CATEGORY_NAME, category)
         }
         startActivity(intent)
+    }
+
+    fun parseJSON(file: String): JSONObject {
+        val inputStream: InputStream = assets.open(file)
+        val json = inputStream.bufferedReader().use { it.readText() }
+        val jsonObj = JSONObject(json)
+        return jsonObj
     }
 }
