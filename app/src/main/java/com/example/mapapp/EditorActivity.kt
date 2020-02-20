2e14ef460a9cfcced34d8f4d1d1c5f0082e155be
jan <fortnitetest@mail.de>
2020-02-20 19:00:59 +0100

    changed EditorActivity layout and fixed some bugs

--- app/src/main/java/com/example/mapapp/EditorActivity.kt
+++ app/src/main/java/com/example/mapapp/EditorActivity.kt
@@ -1,17 +1,54 @@
 package com.example.mapapp
 
-import android.content.Intent
-import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.GridView
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
-import org.json.JSONObject
-import java.io.InputStream
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ExpandableListView
+import android.widget.TextView
+import androidx.annotation.RequiresApi
+import kotlinx.android.synthetic.main.activity_editor.*
+
 
 class EditorActivity: AppCompatActivity() {
 
+    val fileMap: MutableMap<String, String> = mutableMapOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_editor)
+                setContentView(R.layout.activity_editor)
+
+        val questionInput: EditText = findViewById(R.id.setQuestionName)
+        val answerInput: EditText = findViewById(R.id.setAnswerName)
+        val addButton: Button = findViewById(R.id.addButton)
+        val valueDisplay: TextView = findViewById(R.id.addedValuesText)
+
+
+        addButton.setOnClickListener {
+            saveInput(questionInput.text.toString(), answerInput.text.toString())
+            emptyInputs(questionInput, answerInput)
+            val valueString: String = showAddedValue(fileMap)
+            valueDisplay.text = valueString}
+    }
+
+    private fun saveInput(question: String, answer: String) {
+        fileMap[question] = answer
+        setFileTitle.isEnabled = false
+    }
+
+    private fun emptyInputs(vararg inputs: EditText) {
+        for (input in inputs) {
+            input.setText("")
+        }
+    }
+
+    private fun showAddedValue(value: Map<String, String>): String {
+        var text = ""
+        value.forEach { (t, u) ->
+            text += t
+            text = "$text | $u \n"
+        }
+        return text
     }
 }
--- app/src/main/java/com/example/mapapp/MainActivity.kt
+++ app/src/main/java/com/example/mapapp/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.example.mapapp
 
-
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_file.*
 import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONObject
 import java.io.InputStream
